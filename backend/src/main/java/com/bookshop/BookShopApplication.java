ff6fff8124f3f27525d2ed05e8d403a0c6d5a4a1
DeSnotAap <winosabelis@gmail.com>
2025-03-23 23:32:44 +0100

    Update BookShopApplication.java

--- backend/src/main/java/com/bookshop/BookShopApplication.java
+++ backend/src/main/java/com/bookshop/BookShopApplication.java
@@ -5,6 +5,21 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BookShopApplication {
+ 
+    @Autowired
+    private Environment environment;
+    
+    @PostConstruct
+    public void showDatabaseInfo() {
+        String dbUrl = environment.getProperty("spring.datasource.url");
+        System.out.println("Database URL: " + dbUrl);
+        
+        // Get the raw Railway URL
+        String railwayUrl = System.getenv("Postgres.DATABASE_URL");
+        System.out.println("Railway Database URL: " + railwayUrl);
+
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(BookShopApplication.class, args);
     }

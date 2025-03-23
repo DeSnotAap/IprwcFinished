02107721354a9f68e8509978b22d1b43f8cb0605
DeSnotAap <winosabelis@gmail.com>
2025-03-23 23:35:25 +0100

    Update BookShopApplication.java

--- backend/src/main/java/com/bookshop/BookShopApplication.java
+++ backend/src/main/java/com/bookshop/BookShopApplication.java
@@ -2,6 +2,9 @@ package com.bookshop;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.Environment;
+import org.springframework.beans.factory.annotation.Autowired;
+import javax.annotation.PostConstruct;
 
 @SpringBootApplication
 public class BookShopApplication {

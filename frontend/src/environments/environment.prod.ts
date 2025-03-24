5e7ccce7c577d4f60ecab23699a6d8476950577f
DeSnotAap <winosabelis@gmail.com>
2025-03-24 01:25:17 +0100

    Update environment.prod.ts

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://nurturing-consideration-production.up.railway.app/api/' // This would be changed to the production API URL in a real deployment
+  apiUrl: 'https://nurturing-consideration-production.up.railway.app/api' // This would be changed to the production API URL in a real deployment
 }; 
